e6182f0d05fd021b23f30b267b78b0b84fb89d2b
rena <renalynch@renalynch.com>
2020-05-19 08:28:18 -0700

    work

--- javascriptPart1/jsp1.js
+++ javascriptPart1/jsp1.js
@@ -33,3 +33,53 @@ console.log(amazingCars)
 console.log(amazingCars.pop());
 console.log(amazingCars.sort());
 console.log(amazingCars.length)
+
+
+var i = 0;
+while (i <= 4){
+    console.log("The value of i is: " + i);
+    i++;
+}
+var myArray = [3, 5, 9, 11, 18, ];
+for (var i = 0; i < myArray.length; i++){
+    if (myArray[i] >= 10) {
+        console.log("The value " + myArray[i] + " is double digits!");
+    }
+    else {
+        console.log("The value " + myArray[i] + " is single digits!");
+    }
+}
+
+var awesomeBirds = ["Bluebird", "hummingbird", "Roadrunner","Dove"];
+for (var i = 0; i< awesomeBirds.length; i++){
+    if(awesomeBirds[i] === "hummingbird"){
+        console.log("Hummingbirds are awesome!");
+    }else{
+        console.log("Not as Kool as a hummingbird");
+    }
+    
+}
+
+function addStrings(string1,string2){
+    return string1 + string2
+}
+console.log(addStrings(3,5));
+
+function calculateNumbers(){
+     numb1 = 13;
+     numb2 =7;
+    var numb3 = numb1 + numb2;
+
+    if (numb3 > 10) {
+        alert("BIG NUMBER");
+    }else{
+        alert("small number");
+    }
+}
+console.log(calculateNumbers())
+
+
+function addTwoStrings(string1, string2){
+   console.log(string1 + string2);
+}
+var myString = addTwoStrings("John ", "Smith");
