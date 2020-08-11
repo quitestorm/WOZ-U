52467bca8812f2fe1ed87bd8d3e2d65215e1b70a
rena <renalynch@renalynch.com>
2020-08-10 22:34:04 -0700

    d

--- morejsconsepts13/js.js
+++ morejsconsepts13/js.js
@@ -3,6 +3,6 @@ let idealCar = {
     color :"blue",
     weight : 3600,
    myCar: function() {
-       console.log("My ideal car is a " + this.name + " with a color of " +  this.color + " but I do not want it to be over " + this.weight + " pounds")
+      document.getElementById("car").innerHTML = "My ideal car is a " + this.name + " with a color of " +  this.color + " but I do not want it to be over " + this.weight + " pounds"
    }}
    idealCar.myCar()
