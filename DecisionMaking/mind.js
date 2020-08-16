31409762eaebb8406f72c0c0952f5d46c9471122
rena <renalynch@renalynch.com>
2020-08-16 12:11:05 -0700

    bew

---
+++ DecisionMaking/mind.js
@@ -0,0 +1,15 @@
+//alert("Whats up??!! ( :")
+
+const howdy = (a,b) => document.getElementById("frog").innerHTML= a+b;
+
+
+
+
+
+howdy(prompt("are you hungry"), prompt("where do you wantto eat?"));
+
+
+
+
+
+const param = (a,b)  => a * b
