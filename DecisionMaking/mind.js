99ff48d0a02f8f7bb62b8bc71252c144696380fa
rena <renalynch@renalynch.com>
2020-08-16 21:49:12 -0700

    cchanges

--- DecisionMaking/mind.js
+++ DecisionMaking/mind.js
@@ -1,15 +1,16 @@
 //alert("Whats up??!! ( :")
 
-const howdy = (a,b) => document.getElementById("frog").innerHTML= a+b;
+const howdy = (a,b) => document.getElementById("frog").innerHTML= a + " " + b;
 
 
 
 
 
-howdy(prompt("are you hungry"), prompt("where do you wantto eat?"));
+howdy(prompt("are you hungry"), prompt("where do you want to eat?"));
 
 
 
 
 
 const param = (a,b)  => a * b
+console.log(param(9,7));
