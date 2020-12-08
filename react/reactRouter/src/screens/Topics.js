d0a8d3a1843c4aa3c3e5504fd16e8ac915c349e2
rena <renalynch@renalynch.com>
2020-12-07 21:11:01 -0800

    started React App

---
+++ react/reactRouter/src/screens/Topics.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Topic from './Topic';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+
+
+
+
+const Topics = () => (
+  <div>
+    <h2>Topics</h2>
+  </div>
+);
+
+export default Topics;
