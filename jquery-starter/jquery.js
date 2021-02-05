a963d11d385c8e59915e022b3ae17a61a378e264
rena <renalynch@renalynch.com>
2021-02-05 12:39:18 -0800

    more

---
+++ jquery-starter/jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".p1").hover(function(){
+        $(this).css("font-family", "Times New Roman");
+    }, function(){
+        $(this).css("font-family", "Georgia");
+    });
+});
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $(".pets").hide();
+    });
+});
