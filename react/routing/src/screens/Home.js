7b57ec715185a67ac0a913dca0f367805dcfd438
rena <44662369+quitestorm@users.noreply.github.com>
2021-06-12 11:12:01 -0700

    delets

--- react/routing/src/screens/Home.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Header from '../components/Header';
-
-const Home = () => <Header title="Home" />;
-
-export default Home;
--- react/routing/src/screens/Topics.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Header from '../components/Header';
-
-const Topics = () => <Header title="Topics" />;
-
-export default Topics;
